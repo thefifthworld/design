35bb88eeea8ca76aa94477d1ec90be4e0b2816e7
Jason Godesky <jefgodesky@gmail.com>
2020-04-29 19:51:01 -0400

    Add initJumpNav to index

--- js/src/index.js
+++ js/src/index.js
@@ -4,6 +4,9 @@ import 'regenerator-runtime/runtime'
 import { ready } from './utils'
 import { initTypography } from './typography'
 
+import initJumpToNav from './components/jump-to-nav'
+
 ready(() => {
   initTypography()
+  initJumpToNav()
 })
