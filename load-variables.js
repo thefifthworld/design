359eb43aaef9db92796d78093bbedf9105faa153
Jason Godesky <jefgodesky@gmail.com>
2020-05-10 21:16:46 -0400

    Standard layout

--- load-variables.js
+++ load-variables.js
@@ -22,6 +22,10 @@ const cornerRatio = Math.round((1 / tokens.corner) * 1000) / 1000
 lines.push('// Border radii')
 lines.push(`$border-radius-long: 1em unquote("/") ${cornerRatio}em;`)
 lines.push(`$border-radius-tall: ${cornerRatio}em unquote("/") 1em;`)
+lines.push(`$border-radius-long-double: 2em unquote("/") ${cornerRatio * 2}em;`)
+lines.push(`$border-radius-tall-double: ${cornerRatio * 2}em unquote("/") 2em;`)
+lines.push(`$border-radius-long-triple: 3em unquote("/") ${cornerRatio * 3}em;`)
+lines.push(`$border-radius-tall-triple: ${cornerRatio * 3}em unquote("/") 3em;`)
 lines.push('')
 
 section('Colors', tokens.colors.palette, key => {
