2023215093e3885b53de606d9ad9ec4efcfafe1d
Jason Godesky <jefgodesky@gmail.com>
2020-05-16 12:34:20 -0400

    Initialize map

--- js/src/components/map/index.js
+++ js/src/components/map/index.js
@@ -1,7 +1,18 @@
-import { removeSelector } from '../../utils'
+import leaflet from 'leaflet'
+import { removeSelector, addClass } from '../../utils'
 
 const initMap = () => {
   removeSelector('.leaflet-wrapper .no-js')
+  const wrappers = Array.from(document.querySelectorAll('.leaflet-wrapper'))
+  if (wrappers) {
+    wrappers.forEach((wrapper, index) => {
+      const id = `leaflet-wrapper-${index}`
+      wrapper.setAttribute('id', id)
+      addClass(wrapper, 'initialized')
+      const map = leaflet.map(id)
+      map.setView(0, 0, 13)
+    })
+  }
 }
 
 export default initMap
