ad041cb7044bedab3c4eec44ef9ecd6c0ffc55d4
Jason Godesky <jefgodesky@gmail.com>
2020-05-17 09:52:58 -0400

    Loading the map

--- js/src/components/map/index.js
+++ js/src/components/map/index.js
@@ -10,7 +10,11 @@ const initMap = () => {
       wrapper.setAttribute('id', id)
       addClass(wrapper, 'initialized')
       const map = leaflet.map(id)
-      map.setView(0, 0, 13)
+      map.setView([ 0, 0 ], 3)
+      leaflet.tileLayer('http://b.tile.stamen.com/terrain-background/{z}/{x}/{y}.png', {
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
+        maxZoom: 18
+      }).addTo(map)
     })
   }
 }
