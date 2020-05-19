4158621bc7ffcd6454030f4923bd9e6d37afda32
Jason Godesky <jefgodesky@gmail.com>
2020-05-18 20:57:23 -0400

    initJumpToNav: Use select

--- js/src/components/jump-to-nav/index.spec.js
+++ js/src/components/jump-to-nav/index.spec.js
@@ -4,27 +4,27 @@ import initJumpToNav from './index'
 
 describe('initJumpToNav', () => {
   it('does nothing if it has no entry point', () => {
-    document.body.innerHTML = '<p>Hello world!</p>'
+    document.body.innerHTML = '<main class="thefifthworld"><p>Hello world!</p></main>'
     initJumpToNav()
-    expect(document.body.innerHTML).toEqual('<p>Hello world!</p>')
+    expect(document.body.innerHTML).toEqual('<main class="thefifthworld"><p>Hello world!</p></main>')
   })
 
   it('inserts nav', () => {
-    document.body.innerHTML = '<main><div class="jump-to-nav"></div><section id="test"><h1>Test</h1></section></main>'
+    document.body.innerHTML = '<main class="thefifthworld"><div class="jump-to-nav"></div><section id="test"><h1>Test</h1></section></main>'
     initJumpToNav()
-    expect(document.body.innerHTML).toEqual('<main><div class="jump-to-nav"><select class="jump-to-nav"><option value="test">Test</option></select></div><section id="test"><h1>Test</h1></section></main>')
+    expect(document.body.innerHTML).toEqual('<main class="thefifthworld"><div class="jump-to-nav"><label for="jump-to-nav">Jump to</label><select class="jump-to-nav" id="jump-to-nav"><option value="test">Test</option></select></div><section id="test"><h1>Test</h1></section></main>')
   })
 
   it('doesn\'t create multiple navs', () => {
-    document.body.innerHTML = '<main><div class="jump-to-nav"></div><section id="test"><h1>Test</h1></section></main>'
+    document.body.innerHTML = '<main class="thefifthworld"><div class="jump-to-nav"></div><section id="test"><h1>Test</h1></section></main>'
     initJumpToNav()
     initJumpToNav()
-    expect(document.body.innerHTML).toEqual('<main><div class="jump-to-nav"><select class="jump-to-nav"><option value="test">Test</option></select></div><section id="test"><h1>Test</h1></section></main>')
+    expect(document.body.innerHTML).toEqual('<main class="thefifthworld"><div class="jump-to-nav"><label for="jump-to-nav">Jump to</label><select class="jump-to-nav" id="jump-to-nav"><option value="test">Test</option></select></div><section id="test"><h1>Test</h1></section></main>')
   })
 
   it('categorizes', () => {
-    document.body.innerHTML = '<main><div class="jump-to-nav"></div><section id="test" data-category="Test"><h1>Test</h1></section></main>'
+    document.body.innerHTML = '<main class="thefifthworld"><div class="jump-to-nav"></div><section id="test" data-category="Test"><h1>Test</h1></section></main>'
     initJumpToNav()
-    expect(document.body.innerHTML).toEqual('<main><div class="jump-to-nav"><select class="jump-to-nav"><optgroup label="Test"><option value="test">Test</option></optgroup></select></div><section id="test" data-category="Test"><h1>Test</h1></section></main>')
+    expect(document.body.innerHTML).toEqual('<main class="thefifthworld"><div class="jump-to-nav"><label for="jump-to-nav">Jump to</label><select class="jump-to-nav" id="jump-to-nav"><optgroup label="Test"><option value="test">Test</option></optgroup></select></div><section id="test" data-category="Test"><h1>Test</h1></section></main>')
   })
 })
