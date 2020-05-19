c14220a62e4f19e0983e359541771eb80828a1ba
Jason Godesky <jefgodesky@gmail.com>
2020-05-18 21:25:19 -0400

    Add documentation for methods, plus initCollapsibles isn't async

--- js/src/components/collapsible/index.js
+++ js/src/components/collapsible/index.js
@@ -1,12 +1,21 @@
 import { select, create, addClass, removeClass, hasClass } from '../../utils'
 
+/**
+ * Event handler for clicking on the toggle handle.
+ * @param event {Object} - The event object.
+ */
+
 const handleToggle = event => {
   const heading = event.target
   const fn = hasClass(heading, 'open') ? removeClass : addClass
   fn(heading, 'open')
 }
 
-const initCollapsibles = async () => {
+/**
+ * Initialize collapsible components
+ */
+
+const initCollapsibles = () => {
   const collapsibles = select('.collapsible')
   collapsibles.forEach(collapsible => {
     const heading = collapsible.querySelector('h1, h2, h3, h4, h5, h6')
