bdf6b6d73a10a6306d99c8513245ed3cf4c7a81c
Jason Godesky <jefgodesky@gmail.com>
2020-05-22 13:43:25 -0400

    Initializing & styling drag-and-drop label

--- js/src/index.js
+++ js/src/index.js
@@ -6,12 +6,14 @@ import { initTypography } from './typography'
 
 import initCollapsibles from './components/collapsible'
 import initFigures from './components/figure'
+import initFileUploads from './components/figure'
 import initJumpToNav from './components/jump-to-nav'
 import initMap from './components/map'
 
 ready(() => {
   initTypography()
   initCollapsibles()
+  initFileUploads()
   initFigures()
   initJumpToNav()
   initMap()
