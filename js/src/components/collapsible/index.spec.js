a2c7016a94436d8eff82a32f87a5dc5d9b122a86
Jason Godesky <jefgodesky@gmail.com>
2020-07-31 19:07:51 -0400

    Get original unit tests working again

--- js/src/components/collapsible/index.spec.js
+++ js/src/components/collapsible/index.spec.js
@@ -1,23 +1,24 @@
 /* global describe, it, expect */
 
+import { select } from '../../utils'
 import initCollapsibles from './index'
 
 describe('initCollapsibles', () => {
   it('does nothing if it has no entry point', () => {
     document.body.innerHTML = '<main class="thefifthworld"><p>Hello world!</p></main>'
-    initCollapsibles()
+    initCollapsibles(select('.collapsible'))
     expect(document.body.innerHTML).toEqual('<main class="thefifthworld"><p>Hello world!</p></main>')
   })
 
   it('initializes collapsible elements', () => {
     document.body.innerHTML = '<main class="thefifthworld"><aside class="collapsible"><h3>Toggle</h3><p>Content.</p></aside></main>'
-    initCollapsibles()
-    expect(document.body.innerHTML).toEqual('<main class="thefifthworld"><aside class="collapsible"><h3 class="toggle closed">Toggle</h3><div class="togglable"><p>Content.</p></div></aside></main>')
+    initCollapsibles(select('.collapsible'))
+    expect(document.body.innerHTML).toEqual('<main class="thefifthworld"><aside class="collapsible"><h3 class="toggle">Toggle</h3><div class="togglable" style="height: 0px;"><p>Content.</p></div></aside></main>')
   })
 
   it('won\'t initialize an element that has no heading', () => {
     document.body.innerHTML = '<main class="thefifthworld"><aside class="collapsible"><p>Content.</p></aside></main>'
-    initCollapsibles()
+    initCollapsibles(select('.collapsible'))
     expect(document.body.innerHTML).toEqual('<main class="thefifthworld"><aside class="collapsible"><p>Content.</p></aside></main>')
   })
 })
