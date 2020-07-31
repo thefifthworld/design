41f28569af219f89500993cbe1e011635135821c
Jason Godesky <jefgodesky@gmail.com>
2020-07-31 18:38:44 -0400

    Style as error if fewer than 30 characters remain

--- js/src/components/charcount/index.js
+++ js/src/components/charcount/index.js
@@ -1,5 +1,14 @@
 import { create, next, hasClass } from '../../utils'
 
+const getCount = (curr, max) => {
+  const remaining = max - curr
+  if (remaining > 30) {
+    return `${curr} characters (${remaining} left)`
+  } else {
+    return `${curr} characters <span class="error">(${remaining} left)</span>`
+  }
+}
+
 /**
  * Initialize character-count textareas.
  * @param textareas {Element[]} - An array of textarea elements to initialize
@@ -10,10 +19,10 @@ const initCharCount = textareas => {
   textareas.forEach(textarea => {
     const max = parseInt(textarea.dataset.charCount)
     const curr = textarea.value.length
-    const remaining = max - curr
+    const txt = getCount(curr, max)
     const n = next(textarea)
     if (!n || !hasClass(n, 'char-count')) {
-      const p = create('p', ['char-count'], null, `${curr} characters (${remaining} left)`)
+      const p = create('p', ['char-count'], null, txt)
       textarea.insertAdjacentElement('afterend', p)
     }
   })
--- js/src/components/charcount/index.spec.js
+++ js/src/components/charcount/index.spec.js
@@ -15,4 +15,10 @@ describe('initCharCount', () => {
     initCharCount(select('textarea[data-char-count]'))
     expect(document.body.innerHTML).toEqual('<main class="thefifthworld"><textarea data-char-count="240"></textarea><p class="char-count">0 characters (240 left)</p></main>')
   })
+
+  it('wraps remaining in error span if fewer than 30 characters remain', () => {
+    document.body.innerHTML = '<main class="thefifthworld"><textarea data-char-count="240">The world has changed. The ruins of ancient cities lie submerged beneath the swollen seas. Beaches of translucent plastic sand mark new boundaries between land and water. Jungles stretch from the equator to the poles.</textarea></main>'
+    initCharCount(select('textarea[data-char-count]'))
+    expect(document.body.innerHTML).toEqual('<main class="thefifthworld"><textarea data-char-count="240">The world has changed. The ruins of ancient cities lie submerged beneath the swollen seas. Beaches of translucent plastic sand mark new boundaries between land and water. Jungles stretch from the equator to the poles.</textarea><p class="char-count">217 characters <span class="error">(23 left)</span></p></main>')
+  })
 })
