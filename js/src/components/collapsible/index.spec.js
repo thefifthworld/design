1b28ce4414d354a02876ff96a0baeb9218352006
Jason Godesky <jefgodesky@gmail.com>
2020-07-31 19:09:47 -0400

    Add unit tests to click the toggle

--- js/src/components/collapsible/index.spec.js
+++ js/src/components/collapsible/index.spec.js
@@ -21,4 +21,21 @@ describe('initCollapsibles', () => {
     initCollapsibles(select('.collapsible'))
     expect(document.body.innerHTML).toEqual('<main class="thefifthworld"><aside class="collapsible"><p>Content.</p></aside></main>')
   })
+
+  it('opens when you click the toggle', () => {
+    document.body.innerHTML = '<main class="thefifthworld"><aside class="collapsible"><h3>Toggle</h3><p>Content.</p></aside></main>'
+    initCollapsibles(select('.collapsible'))
+    const toggle = document.querySelector('.toggle')
+    toggle.dispatchEvent(new Event('click'))
+    expect(document.body.innerHTML).toEqual('<main class="thefifthworld"><aside class="collapsible"><h3 class="toggle open">Toggle</h3><div class="togglable" style="height: 0px;"><p>Content.</p></div></aside></main>')
+  })
+
+  it('opens when you click the toggle a second time', () => {
+    document.body.innerHTML = '<main class="thefifthworld"><aside class="collapsible"><h3>Toggle</h3><p>Content.</p></aside></main>'
+    initCollapsibles(select('.collapsible'))
+    const toggle = document.querySelector('.toggle')
+    toggle.dispatchEvent(new Event('click'))
+    toggle.dispatchEvent(new Event('click'))
+    expect(document.body.innerHTML).toEqual('<main class="thefifthworld"><aside class="collapsible"><h3 class="toggle">Toggle</h3><div class="togglable" style="height: 0px;"><p>Content.</p></div></aside></main>')
+  })
 })
