c62b753eb4e9827ac44c2989ceb00c3096ff14bb
Jason Godesky <jefgodesky@gmail.com>
2020-08-01 10:15:48 -0400

    Get original unit tests to work again

--- js/src/components/map/index.spec.js
+++ js/src/components/map/index.spec.js
@@ -1,17 +1,18 @@
 /* global describe, it, expect */
 
+import { select } from '../../utils'
 import initMap from './index'
 
 describe('initMap', () => {
   it('does nothing if it has no entry point', () => {
     document.body.innerHTML = '<p>Hello world!</p>'
-    initMap()
+    initMap(select('.leaflet-wrapper'))
     expect(document.body.innerHTML).toEqual('<p>Hello world!</p>')
   })
 
   it('removes no-js entries in leaflet-wrapper', () => {
     document.body.innerHTML = '<main class="leaflet-wrapper"><div class="no-js">No JS</div></main>'
-    initMap()
+    initMap(select('.leaflet-wrapper'))
     const check = document.querySelector('.no-js')
     expect(check).toEqual(null)
   })
