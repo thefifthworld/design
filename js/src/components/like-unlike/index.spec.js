bc233ba5dcab7bbc0604292853736b5d5332d19f
Jason Godesky <jefgodesky@gmail.com>
2020-12-30 17:10:58 -0500

    Like/unlike: Fix failing tests

--- js/src/components/like-unlike/index.spec.js
+++ js/src/components/like-unlike/index.spec.js
@@ -11,19 +11,19 @@ describe('initLikes', () => {
   })
 
   it('registers your like', () => {
-    document.body.innerHTML = '<main class="thefifthworld"><p class="likes"><a href="/components.html" class="like" data-unlike="/templates.html">Like</a><span class="count">100</span> likes</p></main>'
+    document.body.innerHTML = '<main class="thefifthworld"><p class="likes"><a href="/components.html" class="like" data-unlike="/templates.html">Like</a><span class="count">100< likes</span></p></main>'
     initLikes(select('.likes'))
     const btn = document.querySelector('a.like')
     btn.dispatchEvent(new Event('click'))
-    expect(document.body.innerHTML).toEqual('<main class="thefifthworld"><p class="likes"><a href="/templates.html" class="unlike" data-unlike="/templates.html" data-like="/components.html">Unlike</a><span class="count">101</span> likes</p></main>')
+    expect(document.body.innerHTML).toEqual('<main class="thefifthworld"><p class="likes initialized"><a href="/templates.html" class="unlike" data-unlike="/templates.html" data-like="/components.html">Unlike</a><span class="count">101<span class="widont">&nbsp;</span>likes</span></p></main>')
   })
 
   it('lets you unlike', () => {
-    document.body.innerHTML = '<main class="thefifthworld"><p class="likes"><a href="/components.html" class="like" data-unlike="/templates.html">Like</a><span class="count">100</span> likes</p></main>'
+    document.body.innerHTML = '<main class="thefifthworld"><p class="likes"><a href="/components.html" class="like" data-unlike="/templates.html">Like</a><span class="count">100 likes</span></p></main>'
     initLikes(select('.likes'))
     const btn = document.querySelector('a.like')
     btn.dispatchEvent(new Event('click'))
     btn.dispatchEvent(new Event('click'))
-    expect(document.body.innerHTML).toEqual('<main class="thefifthworld"><p class="likes"><a href="/components.html" class="like" data-unlike="/templates.html" data-like="/components.html">Like</a><span class="count">100</span> likes</p></main>')
+    expect(document.body.innerHTML).toEqual('<main class="thefifthworld"><p class="likes initialized"><a href="/components.html" class="like" data-unlike="/templates.html" data-like="/components.html">Like</a><span class="count">100<span class="widont">&nbsp;</span>likes</span></p></main>')
   })
 })
