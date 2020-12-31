2097363a179cab253c46d3ff692121d9fd37f4f5
Jason Godesky <jefgodesky@gmail.com>
2020-12-30 19:19:36 -0500

    Don't typogrify textareas

--- js/src/typography.spec.js
+++ js/src/typography.spec.js
@@ -8,4 +8,10 @@ describe('initTypography', () => {
     initTypography()
     expect(document.body.innerHTML).toEqual('<div class="thefifthworld"><p><span class="dquo">“</span>We’ve got apostrophes, and quotes” — and we’ve got<span class="widont">&nbsp;</span>dashes!</p></div>')
   })
+
+  it('does not apply typographical enhancements inside of textareas', () => {
+    document.body.innerHTML = '<div class="thefifthworld"><textarea>"This is a quote." We might refer to the API.</textarea></div>'
+    initTypography()
+    expect(document.body.innerHTML).toEqual('<div class="thefifthworld"><textarea>"This is a quote." We might refer to the API.</textarea></div>')
+  })
 })
