acf674cc50344d3db54dcd2af34e76bfe9fd81b2
Jason Godesky <jefgodesky@gmail.com>
2021-01-21 15:04:41 -0500

    Run pulse demo form

--- js/src/components/pulse/index.js
+++ js/src/components/pulse/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import Cookies from 'js-cookie'
 import jsonwebtoken from 'jsonwebtoken'
-import { addClass, removeClass, closest } from '../../utils'
+import { addClass, removeClass, closest, create } from '../../utils'
 
 /**
  * Writes an appropriate error message to the pulse element.
@@ -86,7 +86,48 @@ const reauth = async (el) => {
 const getExpiration = jwt => {
   const user = jsonwebtoken.decode(jwt)
   const now = new Date()
-  return (user.exp * 1000) - now.getTime()
+  return user ? (user.exp * 1000) - now.getTime() : 0
+}
+
+/**
+ * Initialize Pulse demo form.
+ */
+
+const initPulseDemo = () => {
+  const el = document.querySelector('.pulse-demo:not(.initialized)')
+  const addNotice = el => {
+    const notice = create('p', [], {}, 'You are now logged in. The pulse component will keep you logged in. You shouldn’t see anything at all, unless something goes wrong with an attempt to reauthenticate you.')
+    el.insertAdjacentElement('afterend', notice)
+  }
+
+  if (el && !Cookies.get('jwt')) {
+    addClass(el, 'initialized')
+    const form = el.querySelector('form')
+    if (form) {
+      form.addEventListener('submit', async event => {
+        event.preventDefault()
+        event.stopPropagation()
+        const email = document.getElementById('pulse-demo-email').value
+        const pass = document.getElementById('pulse-demo-passphrase').value
+        try {
+          const res = await axios.post(form.dataset.auth, { email, pass })
+          Cookies.set('jwt', res.data)
+          removeClass(el, 'initialized')
+          addNotice(el)
+        } catch (err) {
+          console.error(err)
+        }
+      })
+    }
+
+    const wrapper = el.querySelector('.buttons')
+    if (wrapper) {
+      const btn = create('button', [], {}, 'Log in')
+      wrapper.appendChild(btn)
+    }
+  } else if (el) {
+    addNotice(el)
+  }
 }
 
 /**
@@ -95,6 +136,7 @@ const getExpiration = jwt => {
  */
 
 const initPulse = elems => {
+  initPulseDemo()
   const el = elems && Array.isArray(elems) && elems.length > 0 ? elems[0] : null
   if (el) {
     addClass(el, 'initialized')
